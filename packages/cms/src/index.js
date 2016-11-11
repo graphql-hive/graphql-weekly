0be233e6d9760357621373b383174eedaf19f24a
Nilan <nilan.marktanner@student.kit.edu>
2016-11-11 14:32:55 +0100

    add authorization to qlator

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -10,8 +10,12 @@ import {Lokka} from 'lokka';
 import {Transport} from 'lokka-transport-http';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+const headers = {
+  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0Nzg4Njk1NDAsImNsaWVudElkIjoiY2lvcTk1b2VjMDJrajAxbzBvbmpvcHBmOSIsInByb2plY3RJZCI6ImNpcGIxMTFwdzVmZ3QwMW8wZTdodngybGYiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNpdmRzcW9zMjBmMnEwMTQ1YnVlMDMzdzAifQ.bVe4_30gcPIqw-mbBxtRY7k3RAc_hyd0Dl_g5pB32JQ'
+}
+
 export const client = new Lokka({
-    transport: new Transport('https://api.graph.cool/simple/v1/cipb111pw5fgt01o0e7hvx2lf')
+    transport: new Transport('https://api.graph.cool/simple/v1/cipb111pw5fgt01o0e7hvx2lf', {headers})
 });
 
 const theme = getMuiTheme({
