6fad7399cc4fe1fef319b62e47fc6ef3b41ba1cf
Nilu <nilubava@gmail.com>
2020-07-28 14:11:37 +0200

    changed cors

--- packages/api/apollo/server/index.ts
+++ packages/api/apollo/server/index.ts
@@ -14,6 +14,8 @@ import { createContext } from '../context'
 
 // prod..
 const server = new ApolloServer({ schema, context: createContext })
-exports.handler = server.createHandler(cors: {
-    origin: '*',
-  });
+exports.handler = server.createHandler({
+    cors: {
+      origin: '*'
+    },
+  })
