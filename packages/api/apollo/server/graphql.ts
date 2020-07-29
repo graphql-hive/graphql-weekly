d61f31d1518cfa9b1f43c9b488d0d3c3a4ae36d7
Nilu <nilubava@gmail.com>
2020-07-29 14:19:43 +0200

    updated topic deletion

--- packages/api/apollo/server/graphql.ts
+++ packages/api/apollo/server/graphql.ts
@@ -1,20 +1,20 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-lambda'
 import { schema } from '../schema'
 import { createContext } from '../context'
 
 // dev
-new ApolloServer({ schema, context: createContext }).listen(
-  { port: 4000 },
-  () =>
-    console.log(
-      `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
-    ),
-)
+// new ApolloServer({ schema, context: createContext }).listen(
+//   { port: 4000 },
+//   () =>
+//     console.log(
+//       `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
+//     ),
+// )
 
 // prod..
-// const server = new ApolloServer({ schema, context: createContext })
-// exports.handler = server.createHandler({
-//   cors: {
-//     origin: '*',
-//   },
-// })
+const server = new ApolloServer({ schema, context: createContext })
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+  },
+})
