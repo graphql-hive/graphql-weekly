a244a90cbf3723c4ccb199f9767785874c7622c7
Ryan Chenkie <ryanchenkie@gmail.com>
2021-07-16 09:52:17 -0400

    add graphcdn ad

--- packages/api/src/email/src/index.ts
+++ packages/api/src/email/src/index.ts
@@ -173,7 +173,7 @@ function renderFooter(isFoundation) {
     return `
     <td valign="top">
       <img
-          src="https://i.imgur.com/kd3SWX8.png"
+          src="https://i.imgur.com/NrjW32Z.png"
           class="ConfImage"
           width="680"
           height="340"
@@ -733,9 +733,9 @@ function formatTemplate(issue: Issue) {
                             >
                               <tr>
                               <td valign="top">
-                                <a href="https://prisma.io/graphql?utm_source=graphqlweekly&utm_medium=email&utm_campaign=graphqlweekly-prisma" target="_blank">
+                                <a href="https://graphcdn.io/" target="_blank">
                                   <img
-                                      src="https://i.imgur.com/kd3SWX8.png"
+                                      src="https://i.imgur.com/NrjW32Z.png"
                                       class="ConfImage"
                                       width="680"
                                       height="340"
