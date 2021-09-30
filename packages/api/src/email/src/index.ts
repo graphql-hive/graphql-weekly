1935661a3020884579721b49fa7361e46f7f0010
Tasin Ishmam <tasinishmam@gmail.com>
2021-09-30 15:45:19 +0600

    Revert "Update image to graphql conf"
    This reverts commit 33952249df1f84a114131de874098a4c83980c41.

--- packages/api/src/email/src/index.ts
+++ packages/api/src/email/src/index.ts
@@ -173,7 +173,7 @@ function renderFooter(isFoundation) {
     return `
     <td valign="top">
       <img
-          src="https://i.imgur.com/Izvnvgm.png"
+          src="https://i.imgur.com/kd3SWX8.png"
           class="ConfImage"
           width="680"
           height="340"
@@ -733,9 +733,9 @@ function formatTemplate(issue: Issue) {
                             >
                               <tr>
                               <td valign="top">
-                                <a href="https://graphqlconf.org/" target="_blank">
+                                <a href="https://prisma.io/" target="_blank">
                                   <img
-                                      src="https://i.imgur.com/Izvnvgm.png"
+                                      src="https://i.imgur.com/kd3SWX8.png"
                                       class="ConfImage"
                                       width="680"
                                       height="340"
