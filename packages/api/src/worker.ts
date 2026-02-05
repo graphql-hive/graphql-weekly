d44c5743bbc198b1439503015fb26ee1f4914896
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-02-05 15:09:08 +0100

    Simplify preview routing: use workers.dev URLs directly

--- packages/api/src/worker.ts
+++ packages/api/src/worker.ts
@@ -13,6 +13,7 @@ export interface Env extends AuthEnv {
   LOCAL_DEV?: string
   MAILCHIMP_API_KEY?: string
   MAILCHIMP_SERVER_PREFIX?: string
+  PREVIEW_API_WORKER_NAME?: string
   WORKERS_DEV_SUBDOMAIN?: string
 }
 
@@ -111,7 +112,9 @@ export default {
       }
 
       const workerName =
-        preview.service === 'api' ? 'graphqlweekly-api' : 'graphqlweekly-cms'
+        preview.service === 'api'
+          ? env.PREVIEW_API_WORKER_NAME || 'graphqlweekly-api'
+          : 'graphqlweekly-cms'
       const previewUrl = `https://${preview.subdomain}-${workerName}.${env.WORKERS_DEV_SUBDOMAIN}.workers.dev${url.pathname}${url.search}`
       const proxyRequest = new Request(previewUrl, {
         body: request.body,
