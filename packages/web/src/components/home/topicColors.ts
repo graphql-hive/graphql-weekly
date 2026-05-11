471d19fdd7a6d3e11f2447606b14e3f825c94e31
Dotan Simha <dotansimha@gmail.com>
2026-05-11 18:56:43 +0300

    fix colors

--- packages/web/src/components/home/topicColors.ts
+++ packages/web/src/components/home/topicColors.ts
@@ -5,13 +5,10 @@ export const topicColors: Record<string, string> = {
   Events: "#009BE3",
   "Community & Events": "#009BE3",
   Conference: "#009BE3",
-  Events: "#009BE3",
-
   "Frameworks and Libraries": "#F0950C",
   Libraries: "#F0950C",
   "Libraries and Tools": "#F0950C",
   "Open Source": "#F0950C",
-
   "Podcasts and Shows": "#27AE60",
   Slides: "#27AE60",
   Talks: "#27AE60",
